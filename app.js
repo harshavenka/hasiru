a3741320afaa808e46527aa96351ee8f102ebff0
harshavenka <harshav.1101@gmail.com>
2022-01-08 21:35:55 +0530

    signup page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const authroutes = require('./routes/auth.routes');
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+app.use(express.static('public'));
+
 app.use(authroutes);
 
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 const authController = require('../controllers/auth.controller');
 //....
 router.get('/signup',authController.getSignup);
